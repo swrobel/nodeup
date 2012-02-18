a70f5460e54795fc32262f87aea20a5880bb8085
Keenahn Jung <keenahn+github@gmail.com>
2012-02-18 23:59:22 +0000

    "added file types"

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
+
 
 http.createServer(function (req, res) {
     fs.readFile(__dirname + req.url, 'utf-8', function(err, data) {
@@ -8,15 +10,24 @@ http.createServer(function (req, res) {
             res.end('File ' + req.url + ' not found\n');
         }
         else {
-            if(req.url.substr(-4, 4) == ".css"){
+            var extname = path.extname(req.url);
+            
+            if(extname == ".css"){
                 mime = "text/css";    
-            } else if(req.url.substr(-4,4) == ".htm" || req.url.substr(-5,5) == ".html"  ){
+            } else if(extname == ".html" || extname == ".html"){
                 mime = "text/html";
-            } else if(req.url.substr(-3,3) == ".js"){
+            } else if(extname == ".js"){
                 mime = "text/javascript";    
+            } else if(extname == ".jpg" || extname == ".jpeg"){
+                mime = "image/jpeg";    
+            } else if(extname == ".gif"){
+                mime = "image/gif";    
+            } else if(extname == ".png"){
+                mime = "image/png";
             } else {
                 mime = "text/plain";    
             }
+
 
             res.writeHead(200, {'Content-Type': mime});
             res.end(data);
